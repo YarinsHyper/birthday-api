b0bb9767d5bb7d83407ecc82f96992608309b812
yarinBenisty <yarin1857@gmail.com>
2021-06-21 20:53:32 +0300

    cr fixes

--- go.mod
+++ go.mod
@@ -4,8 +4,7 @@ go 1.16
 
 require (
 	github.com/gin-gonic/gin v1.7.2
-	github.com/google/uuid v1.2.0
-	github.com/gorilla/mux v1.8.0 // indirect
-	github.com/yarinBenisty/birthday-service v0.0.0-20210614140716-a77406663693 // indirect
-	google.golang.org/grpc v1.38.0 // indirect
+	github.com/joho/godotenv v1.3.0
+	github.com/yarinBenisty/birthday-service v0.0.0-20210616075813-f3277f0afda1
+	google.golang.org/grpc v1.38.0
 )
--- main.go
+++ main.go
@@ -3,105 +3,129 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 	bpb "github.com/yarinBenisty/birthday-service/proto"
 	"google.golang.org/grpc"
 )
 
-//router that's connecting to the client
-//functions in the bd-service proto file
+const (
+	name           = "name"
+	personalNumber = "personalNumber"
+	date           = "date"
+)
+
+// Router that's connecting to the client
+// functions in the bd-service proto file
 type Router struct {
 	client bpb.BirthdayFunctionsClient
 }
 
-//function that creates a new client and connects
-//to the bd-service client through the proto file
+// Func that creates a new client and connects
+// to the bd-service client through the proto file
 func initClientConnection() bpb.BirthdayFunctionsClient {
+
+	envError := godotenv.Load()
+	if envError != nil {
+		log.Fatal("Error loading .env file! error: ", envError)
+	}
+
+	address := os.Getenv("ADDRESS") + os.Getenv("PORT")
 	conn, err := grpc.Dial(
-		"localhost:8000",
+		address,
 		grpc.WithInsecure(),
 		grpc.FailOnNonTempDialError(true),
 		grpc.WithBlock(),
 	)
 	if err != nil {
-		log.Fatalln("error: ", err)
+		log.Fatalln("connection error: ", err)
+		os.Exit(4)
 	}
-	defer conn.Close()
+	// defer conn.Close()
 
 	client := bpb.NewBirthdayFunctionsClient(conn)
 
 	return client
 }
 
-//function that inserts a birthday object
+//CreateBirthday inserts a birthday object
 func (r *Router) CreateBirthday(c *gin.Context) {
-	name := c.Request.FormValue("name")
-	personalNumber := c.Request.FormValue("personalNumber")
-	date := c.Request.FormValue("date")
 
 	request := &bpb.CreateBirthdayRequest{
-		PersonalNumber: personalNumber, Name: name, Date: date}
+		PersonalNumber: c.Request.FormValue(personalNumber),
+		Name:           c.Request.FormValue(name),
+		Date:           c.Request.FormValue(date),
+	}
+
 	res, err := r.client.CreateBirthday(c, request)
 	if err != nil {
-		fmt.Println("create birthday method failed. error: ", err)
+		fmt.Println("create birthday error: ", err)
+		c.String(400, "create birthday method failed. \nerror: %s", err)
+		os.Exit(3)
 	}
-
-	c.JSON(200, res)
+	c.JSON(201, res)
 }
 
-//function that read/get all birthday objects
+// GetBirthday read/get all birthday objects
 func (r *Router) GetBirthday(c *gin.Context) {
-	personalNumber := c.Query("personalNumber")
 
-	request := &bpb.GetBirthdayRequest{PersonalNumber: personalNumber}
+	request := &bpb.GetBirthdayRequest{PersonalNumber: c.Query(personalNumber)}
 	res, err := r.client.GetBirthday(c, request)
 	if err != nil {
-		fmt.Println("get birthday method failed. error: ", err)
+		fmt.Println("get birthday error: ", err)
+		c.String(404, "get birthday method failed. \nerror: %s", err)
+		os.Exit(5)
 	}
-
 	c.JSON(200, res)
 }
 
-//function that updates a birthday object by personalNumber
+// UpdateBirthday updates a birthday object by personalNumber
 func (r *Router) UpdateBirthday(c *gin.Context) {
-	personalNumber := c.Request.FormValue("personalNumber")
-	name := c.Request.FormValue("name")
-	date := c.Request.FormValue("date")
 
 	request := &bpb.UpdateBirthdayRequest{PersonalNumber: personalNumber, Name: name, Date: date}
 	res, err := r.client.UpdateBirthday(c, request)
 	if err != nil {
-		fmt.Println("update birthday method failed. error: ", err)
+		fmt.Println("update birthday error: ", err)
+		c.String(400, "update birthday method failed. \nerror: %s", err)
+		os.Exit(5)
 	}
-	c.JSON(200, res)
+	c.JSON(201, res)
 }
 
-//function that deletes a certain birthday object by personal number
+// DeleteBirthday deletes a certain birthday object by personal number
 func (r *Router) DeleteBirthday(c *gin.Context) {
-	personalNumber := c.Query("personalNumber")
 
 	request := &bpb.DeleteBirthdayRequest{PersonalNumber: personalNumber}
 	res, err := r.client.DeleteBirthday(c, request)
 	if err != nil {
-		fmt.Println("delete birthday method failed. error: ", err)
+		fmt.Println("delete birthday error: ", err)
+		c.String(404, "delete birthday method failed. \nerror: %s", err)
+		os.Exit(5)
 	}
-
-	c.JSON(200, res)
+	c.JSON(204, res)
 }
 
 func main() {
+
+	envError := godotenv.Load()
+	if envError != nil {
+		log.Fatal("Error loading .env file! error: ", envError)
+	}
+	routerPort := os.Getenv("ROUTER_PORT")
+
 	r := &Router{}
 	r.client = initClientConnection()
 
 	mainRouter := gin.Default()
 	mainRouter.POST("/api/createBirthday", r.CreateBirthday)
-	mainRouter.GET("/api/getBirthday/query", r.GetBirthday)
+	mainRouter.GET("/api/getBirthday", r.GetBirthday)
 	mainRouter.POST("/api/updateBirthday", r.UpdateBirthday)
-	mainRouter.DELETE("/api/deleteBirthday/query", r.DeleteBirthday)
+	mainRouter.DELETE("/api/deleteBirthday", r.DeleteBirthday)
 
-	err := mainRouter.Run(":9000")
+	err := mainRouter.Run(":" + routerPort)
 	if err != nil {
-		log.Fatalln("failed to run api-gateway router. error: ", err)
+		log.Fatalln("failed to run api-gateway router. \nerror: ", err)
 	}
 }
