dfd65475e69fc6d29833c183b1fba59cec7eb3e0
yarinBenisty <yarin1857@gmail.com>
2021-07-01 17:05:00 +0300

    fix: small changes

--- main.go
+++ main.go
@@ -23,6 +23,7 @@ const (
 // functions in the bd-service proto file
 type Router struct {
 	client bpb.BirthdayFunctionsClient
+	bpb.UnimplementedBirthdayFunctionsServer
 }
 
 // Func that creates a new client and connects
@@ -75,7 +76,7 @@ func corsRouterConfig() cors.Config {
 
 //CreateBirthday inserts a birthday object
 func (r *Router) CreateBirthday(c *gin.Context) {
-
+	var code = 201
 	request := &bpb.CreateBirthdayRequest{
 		PersonalNumber: c.Request.FormValue(personalNumber),
 		Name:           c.Request.FormValue(name),
@@ -83,63 +84,63 @@ func (r *Router) CreateBirthday(c *gin.Context) {
 	}
 	res, err := r.client.CreateBirthday(c, request)
 	if err != nil {
+		code = 400
 		fmt.Println("create birthday error: ", err)
-		c.String(400, "create birthday method failed. \nerror: %s", err)
-		os.Exit(3)
+		c.String(code, "create birthday method failed. \nerror: %s", err)
 	}
-	c.JSON(201, res)
+	c.JSON(code, res)
 }
 
 // GetBirthday get a birthday object
 func (r *Router) GetBirthday(c *gin.Context) {
-
+	var code = 200
 	request := &bpb.GetBirthdayRequest{PersonalNumber: c.Query(personalNumber)}
 	res, err := r.client.GetBirthday(c, request)
 	if err != nil {
+		code = 404
 		fmt.Println("get birthday error: ", err)
-		c.String(404, "get birthday method failed. \nerror: %s", err)
-		os.Exit(5)
+		c.String(code, "get birthday method failed. \nerror: %s", err)
 	}
-	c.JSON(200, res)
+	c.JSON(code, res)
 }
 
 //GetAllBirthday gets all birthday objects
 func (r *Router) GetAllBirthday(c *gin.Context) {
-
+	var code = 200
 	request := &bpb.GetAllBirthdayRequest{}
 	res, err := r.client.GetAllBirthday(c, request)
 	if err != nil {
-		fmt.Println("get all birthday error: ", err)
-		c.String(404, "get all birthday method failed. \nerror: %s", err)
-		os.Exit(5)
+		code = 404
+		fmt.Println("get all birthday error.\n", err)
+		c.String(code, "get all birthday method failed. \nerror: %s", err)
 	}
-	c.JSON(200, res)
+	c.JSON(code, res)
 }
 
 // UpdateBirthday updates a birthday object by personalNumber
 func (r *Router) UpdateBirthday(c *gin.Context) {
-
+	var code = 201
 	request := &bpb.UpdateBirthdayRequest{PersonalNumber: personalNumber, Name: name, Date: date}
 	res, err := r.client.UpdateBirthday(c, request)
 	if err != nil {
+		code = 400
 		fmt.Println("update birthday error: ", err)
-		c.String(400, "update birthday method failed. \nerror: %s", err)
-		os.Exit(5)
+		c.String(code, "update birthday method failed. \nerror: %s", err)
 	}
-	c.JSON(201, res)
+	c.JSON(code, res)
 }
 
 // DeleteBirthday deletes a certain birthday object by personal number
 func (r *Router) DeleteBirthday(c *gin.Context) {
-
+	var code = 204
 	request := &bpb.DeleteBirthdayRequest{PersonalNumber: personalNumber}
 	res, err := r.client.DeleteBirthday(c, request)
 	if err != nil {
+		code = 404
 		fmt.Println("delete birthday error: ", err)
-		c.String(404, "delete birthday method failed. \nerror: %s", err)
-		os.Exit(5)
+		c.String(code, "delete birthday method failed. \nerror: %s", err)
 	}
-	c.JSON(204, res)
+	c.JSON(code, res)
 }
 
 func main() {
